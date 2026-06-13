c8b7a3b2bab57da2b9e4faf3c206abc443899398
razannabilWebDev <razannabilannadif@gmail.com>
2026-06-13 15:54:00 +0700

    update some stuff

--- config/koneksi.php
+++ config/koneksi.php
@@ -6,36 +6,8 @@ $db   = "warung_kelontong";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
-<<<<<<< HEAD
 // Cek apakah koneksi berhasil
 if (!$conn) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
-
-mysqli_set_charset($conn, "utf8mb4");
-
-function prepared_query($conn, $sql, $types = "", $params = [])
-{
-    $stmt = mysqli_prepare($conn, $sql);
-
-    if (!$stmt) {
-        die("Prepare query gagal: " . mysqli_error($conn));
-    }
-
-    if ($types !== "" && !empty($params)) {
-        mysqli_stmt_bind_param($stmt, $types, ...$params);
-    }
-
-    if (!mysqli_stmt_execute($stmt)) {
-        die("Eksekusi query gagal: " . mysqli_stmt_error($stmt));
-    }
-
-    return $stmt;
-}
 ?>
-=======
-if(!$conn){
-    die("Koneksi gagal");
-}
-?>
->>>>>>> feature/inventori
